52f3cabeff1c03ee201fdb4ea0ef97674590f4c4
Baker Renneckar <bakerrenneckar@gmail.com>
2018-04-10 10:38:04 -0400

    Added Site Coordination file to repo

---
+++ site_coordination.R
@@ -0,0 +1,21 @@
+macdatawd <- "/Volumes/GoogleDrive/My Drive/Data Files"
+windowsdatawd <- "C:/Users/USER/Google Drive/Data Files"
+if(file.exists(macdatawd)){
+  setwd(file.path(macdatawd))
+} else { 
+  if(file.exists(windowsdatawd)){
+    setwd(file.path(windowsdatawd))
+  }
+}
+
+site_coordination <-readWorksheetFromFile('site_coordination.xlsx', sheet=1, header = T, startRow = 1)
+
+#site_coordination <- data.frame(apply(site_coordination, 2, function(x) gsub("^$|^ $", NA, x)))
+#site_coordination  <- site_coordination[,colSums(is.na(site_coordination))<nrow(site_coordination)]
+
+site_coordination <- site_coordination[!is.na(site_coordination$School), ]
+
+#colnames(site_coordination)[15:18] <- c("students_served", "parents_served", "other_served", "volunteers")
+
+write.csv(site_coordination, "site_coordination.csv")
+
